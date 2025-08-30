1068b94b42ea02b7d1a470bb03e331486e0f904b
Suryamani kumar <124435021+suryamani752@users.noreply.github.com>
2025-08-30 08:45:50 +0530

    mera-pahla-commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const cricketPlayer = {
+  name: "Virat Kohli",
+  age: 38,
+  ipl: "RCB",
+  batsmen: "Right handed",
+  bowling: "spin off break",
+};
+// console.log(cricketPlayer.bowling);
+
+const keys = Object.keys(cricketPlayer);
+// console.log(keys);
+const values = Object.values(cricketPlayer);
+// console.log(values);
+values.forEach(value => {
+  
+})
+const keyValue = Object.entries(cricketPlayer);
+console.log(keyValue);
+for(const [key,value] of keyValue){
+  
+}
